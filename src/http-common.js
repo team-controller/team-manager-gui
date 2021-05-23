833fb34eeda878026c3ce13a5fdf52a879563eca
Amine <amichachi@alum.us.es>
2021-05-23 19:45:44 +0200

    Update http-common

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://8080-9b15a2a9-8e12-4e86-8e4f-0fdf50440277.cs-europe-west1-akef.cloudshell.dev/api/",
   headers: {
     "Content-type": "application/json"
   }
